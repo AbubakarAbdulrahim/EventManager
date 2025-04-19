a76eea403028a03b4d1f7bde300b92a1f99f24fe
sssnewplus <habibudanlamidahir@gmail.com>
2025-04-19 19:24:04 +0100

    added cors origin

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -146,6 +146,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # cors headers stuffs 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWS_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
 
 
 SESSION_COOKIE_SECURE = False
