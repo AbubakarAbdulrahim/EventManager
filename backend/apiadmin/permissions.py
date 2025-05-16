2472e73da4ba038b45181c1cf545fc87043c239d
Sadeeq <sadeejert@gmail.com>
2025-05-16 10:58:52 +0100

    Remove backend folder from repo and ignore it

--- backend/apiadmin/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-# custom admin permission
-class IsAdminRole(BasePermission):
-
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.role == 'admin'
-
-
--- backend/transactions/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import TransactionListCreateView, TransactionRetrieveView
-
-
-urlpatterns = [
-    path('', TransactionListCreateView.as_view(), name='trxn_list_create'),
-    path('<int:pk>/', TransactionRetrieveView.as_view(), name='trxn_detail'),
-]
