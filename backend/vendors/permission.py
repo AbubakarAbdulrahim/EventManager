52ed5b24ab55ab6019afc02b30faddac7d0ec6d2
sadeeq <sadeejert@gmail.com>
2025-07-11 05:28:45 +0100

    reset

--- backend/vendors/permission.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-# custom vendor permission
-class IsVendorRole(BasePermission):
-
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.role == 'vendor' and request.user.is_active
